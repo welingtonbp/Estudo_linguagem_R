8374514ba13186cab666c2aba3dfc03b3550b79f
welingtonbp <welington.pereira@qi3.com.br>
2015-05-15 11:37:26 -0300

    Anotações aula 4
    Conceitos de Diretório do R, e comendos referentes.

---
+++ Primeiro-Script.R
@@ -0,0 +1,26 @@
+#--------------------------------------------------------------------------------------------------
+# Comandos básicos no R
+#
+# Gorgens, Eric Bastos
+# Department of Forest Sciences
+# University of Sao Paulo
+# Piracicaba, Brazil
+# gorgens (at) usp.br
+#
+#--------------------------------------------------------------------------------------------------
+
+# Aula 4
+
+# Onde procurar ajuda
+# Melhor amigo do programador:
+# http://stackoverflow.com/
+# Segundo melhor amigo:
+# http://www.google.com
+# E ainda vc pode:
+# Foruns especializados em R
+# Help do R
+
+#--------------------------------------------------------
+# Manipulando o diretório de trabalho
+getwd() # mostra o diretório de trabalho atual
+
