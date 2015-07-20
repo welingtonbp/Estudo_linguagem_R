c1408403856f0178caea0cd9e9bc71d070de9f96
Welington Batista Pereira <welington.pereira@qi3.com.br>
2015-07-20 20:58:21 -0300

    Update

--- sapply.R
+++ sapply.R
@@ -1,14 +1,17 @@
-## SAPPLY
-# O objetivo é simplificar o resultado da função "lapply"
-
-x <- list(a = 1:4, b = rnorm(10), c = rnorm(20,1), d = rnorm(100,5))
-
-# GErando lista com "lapply"
-lapply(x,mean)
-
-## Comparação com a função "sapply"
-sapply(x,mean)
-
-mean(x)
-## Obs: a função "mean" não deve ser aplicada em listas
+# Funçao Sapply
+#
+# Essa função é somente uma variação da lapply. Ele tenta simplificar o resultado da lapply 
+# Exemplo: se o resultado é uma list em que todos os elementos são vetores de mesmo comprimento,
+# Colocará os elementos em uma matrix 
+#
+# Primeiro aplicando o lapply
+#
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100,5))
+lapply(x, mean)
+#
+# Agora demonstrando o Sapply
+#
+sapply(x, mean)
+#
+mean(x)  ## Vai dar erro pois a funçao mean, nao deve ser aplicada em list
 
