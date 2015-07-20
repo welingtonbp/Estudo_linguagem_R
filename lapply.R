e1324a04e2ba9c548d309116ed40d4afa5269a97
Welington Batista Pereira <welington.pereira@qi3.com.br>
2015-07-20 20:59:13 -0300

    Update

--- lapply.R
+++ lapply.R
@@ -1,26 +1,33 @@
-# Looping on the Command Line
-
-# lapply; sapply; apply; tapply; mapply
-
-# 1º) lapply
-# Recebe três argumentos
-## O primeiro é uma lista chamada de x
-## O segundo é uma função ou nome de uma função, entre outros argumentos
-## 
-# Sempre vai retornar uma lista
-
+# Loop function - LAPPLY
+#
+# Recebe três argumentos: | LISTA chamada de X | função ou nome de uma FUNÇÃO | e outros argumentos que podem ser passados pelo argumento retissencias (serve para passar argumentos desta função)
+#
+# Exemplo
 x <- list(a = 1:5, b = rnorm(10))
 lapply(x, mean)
-
-#----------------------------------
-
-# --- Usando matrix
-
+#
+# Exemplo 2
+#
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100,5))
+lapply(x, mean)
+#
+# Exemplo 3
+# Obs: runif são variáveis aleatórias de distribuição uniforme
+#
+x <- 1:4
+lapply(x, runif)
+#
+# Exemplo 4
+# Funções anônimas: são aquelas que não tem nomes, portanto você não atribui a elas nomes de qualquer tipo
+#
 x <- list(a = matrix(1:4, 2, 2), b = matrix(1:6, 3, 2))
 x
-
-# Extraindo somente a primeira coluna
+# Extraindo a primeira coluna de uma matrix, com função anônima
+# A "function(elt) elt[,1]" está ai somente para filtrar a primeira coluna de cada matrix
+#
 lapply(x, function(elt) elt[,1])
+#
+
 
 
 
