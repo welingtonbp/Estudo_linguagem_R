a38407d37eec7b045a82365e54d4a06f3bb906f4
Welington Batista Pereira <welington.pereira@qi3.com.br>
2015-07-21 08:43:44 -0300

    Update

--- mapply.R
+++ mapply.R
@@ -1,3 +1,23 @@
+# Loop function - Lappay
+# A mapply é uma função de loop que é uma versão multivariada da funções lapply e sapply
+#
+# a idéia é colar uma função em paralelo sobre um conjunto de diferentes argumentos
+#
+# Explicando a função
+str(mapply)
+# O primeiro argumento de mapply é a função que você quer aplicar
+# O segundo argumento são as reticencias, pois referem-se as variáveis necessárias para a função
+# o terceiro MoreArgs é usado somente quando tem-se mais argumentos e precisa passar para sua função
+# o quarto SIMPLIFY é similar aos argumentos que tem em sapply e tapply
+str(mapply)
+function(FUN, ..., MoreArgs = NULL, SIMPLIFY, USE.NAMES = TRUE)
+#
+# Exemplo 2
+#
+# MAPPLY pode ser usado para aplicar uma função à muitos conjuntos de argumentos
+#
+# ------------------------------------------------------
+
 # mapply
 # é uma uma função de loop que é uma versão multivariada
 
